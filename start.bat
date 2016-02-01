8ddaf8ab4267ab018cdade24063b3964b708c9ad
unknown <haotianyi@568C2B5E51BF95D.internal.baidu.com>
2016-02-01 05:11:23 +0000

    update results

--- start.bat
+++ start.bat
@@ -1,5 +1,20 @@
-python merge.py %1
+python merge.py 1234hta
 txt2pgn.exe
 bayeselo.exe
-python merge_version.py %1 %2 1
-python merge_version.py %1 %2 0
+python merge_version.py 1234hta 50 1
+python merge_version.py 1234hta 50 0
+python merge.py f
+txt2pgn.exe
+bayeselo.exe
+python merge_version.py f 100 1
+python merge_version.py f 100 0
+python merge.py s
+txt2pgn.exe
+bayeselo.exe
+python merge_version.py s 20 1
+python merge_version.py s 20 0
+python merge.py 1234htafs
+txt2pgn.exe
+bayeselo.exe
+python merge_version.py 1234htafs 100 1
+python merge_version.py 1234htafs 100 0
